1645fe4e28fdccd861835c9529dbd42655b16a49
Galimimus <galimimus37@gmail.com>
2022-12-12 00:07:07 +0300

    First steps

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>signup</title>
+        <style>
+            table {
+             width: 300px;
+             margin: auto; 
+             margin-top:20%;
+             border-spacing: 10px 20px;
+            }
+            td {
+             text-align: center; 
+             padding: 5px; 
+            }
+            label{
+                color: blue;
+            }
+            .block1{
+                color: red;
+
+            }
+        </style>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    </head>
+    <body>
+            <table>
+                <tr>
+                    <td colspan="2"> 
+                        <div class="block1">
+                            <?php
+                            session_start();
+                            if(isset($_SESSION["msg"])){
+                                print($_SESSION["msg"]);
+                                session_destroy();
+                            }
+                            ?>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <div class="form-group">
+                        <td>
+                            <a href = "lk_employees.php">Личный кабинет сотрудников</href>
+                        </td>
+                    </div>
+                </tr>
+                <tr>
+                    <div class="form-group">
+                        <td>
+                            <a href = "lk_students.php">Личный кабинет учащихся</href>
+                        </td>
+                    </div>
+                </tr>
+        </table>
+    </body>
+</html>
---
+++ lk_students.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>login</title>
+        <style>
+            table {
+             width: 300px;
+             margin: auto; 
+             margin-top:20%;
+             border-spacing: 10px 20px;
+            }
+            td {
+             text-align: center; 
+             padding: 5px; 
+            }
+            label{
+                color: blue;
+            }
+            .block1{
+                color: red;
+            }
+        </style>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    </head>
+    <body>
+        
+        <form method="POST" action="db/students.php" class="form-inline">
+            <table>
+            <tr>
+                    <td colspan="2">
+                        <label>Вход в личный кабинет учащихся</label>
+                    </td>
+                </tr>
+                <tr>
+                    <div class="form-group">
+                        <td>
+                            <label>ФИО</label>
+                        </td>
+                        <td>
+                            <input type="text" name="fio" class="form-control" required/>
+                        </td>
+                    </div>
+                </tr>
+                <tr>
+                    <div class="form-group">
+                        <td>
+                            <label for="pwd">Пароль</label>
+                        </td>
+                        <td>
+                            <input type="password" name="pass" class="form-control" maxlength="30" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                             title="Пароль должен быть введен на английскрм языке, содержать не менее 8 символов и не более 30, буквы верхнего и нижнего регистра, как минимум одну цифру."
+                              required/>
+                        </td>
+                    </div>
+                </tr>
+                <tr>
+                    <td colspan = "2">
+                        <input type="submit" value="Войти" class="btn btn-primary"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <a href = "recovery.php" class="block1">Forgot your password?</href>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
